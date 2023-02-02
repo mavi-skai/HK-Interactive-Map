1726ab70e3515f3549d1b4559a376dd08648b4a1
mavey14 <harveylencio@gmail.com>
2023-02-02 15:53:08 +0800

    markerupdate

--- public/index.js
+++ public/index.js
@@ -1,4 +1,58 @@
+
 window.addEventListener('load', function () {
+    //#region 
+    let grubXY = [[-575, 1314],
+                   [-704, 1030],
+                   [-1015, 860],
+                   [-1067, 1085],]
+                  //  [-867, 1624],
+                  //  [-978, 1569],
+                  //  [-1200,1562],
+                  //  [-1141, 502],
+                  //  [-1400, 836],
+                  //  [-1819, 317],
+                  //  [-1952, 851],
+                  //  [-1891, 1284],
+                  //  [-2093, 1252],
+                  //  [-1625, 1596],
+                  //  [-1520, 1483],
+                  //  [-1434, 1878],
+                  //  [-923, 1733],
+                  //  [-753, 2111],
+                  //  [-705, 2571],
+                  //  [-829, 2396],
+                  //  [-897, 2452],
+                  //  [-648, 2290],
+                  //  [-695, 2808],
+                  //  [-598, 2888],
+                  //  [-726, 2933],
+                  //  [-721, 3308],
+                  //  [-597, 3056],
+                  //  [-431, 3392],
+                  //  [-981, 3556],
+                  //  [-1313, 2516],
+                  //  [-1479, 2744],
+                  //  [-1458, 2994],
+                  //  [-1696, 3265],
+                  //  [-1503, 3588],
+                  //  [-1509, 3607],
+                  //  [-1513, 3626],
+                  //  [-1825, 2558],
+                  //  [-2168, 2206],
+                  //  [-2152, 2560],
+                  //  [-1763, 3395],
+                  //  [-1864, 3391],
+                  //  [-1729, 3573],
+                  //  [-1909, 3564],
+                  //  [-1715, 4001],
+                  //  [-1913, 3916],
+                  //  [-1942, 4385],];
+    let grubMarkers = []
+    let grubMarkerGroup = L.featureGroup();
+    
+    //#endregion
+    
+    
     let mapExtent = [0.00000000, -2901.00000000, 4498.00000000, 0.00000000];
     let mapMinZoom = 0;
     let mapMaxZoom = 3;
@@ -17,7 +71,7 @@ window.addEventListener('load', function () {
         return Math.log(scale * mapMinResolution) / Math.LN2;
     };
 
-    var map = new L.Map('map', {
+    map = new L.Map('map', {
         maxZoom: mapMaxZoom,
         minZoom: mapMinZoom,
         crs: crs,
@@ -44,41 +98,74 @@ window.addEventListener('load', function () {
       ]);
       
     L.control.mousePosition().addTo(map)
+
+    
+    
+
+    
+     
+      createMarkers('icon/grub-icon.png',[40,40],grubXY,'Grub',grubMarkers,grubMarkerGroup)
+      grubMarkers.forEach(marker => grubMarkerGroup.addLayer(marker));
+      map.addLayer(grubMarkerGroup);
+
     
     
 
 });
 
 
-//#region
-// //For example, to change the position of
-// //the zoom control to the top left corner of the map:
-// var zoomControl = new L.Control.Zoom({position: 'topleft'});
-// map.addControl(zoomControl);
 
-// //To change the zoom in and zoom out icons, you can use the zoomInText
-// // and zoomOutText options when creating the zoom control object.
-// var zoomControl = new L.Control.Zoom({
-//     position: 'topright',
-//     zoomInText: '+',
-//     zoomOutText: '-'
-// });
-// map.addControl(zoomControl);
 
-// //You could also use your own icons or images.
-// var zoomControl = L.control.zoom({
-//     position: 'topright',
-//     zoomInText: '<i class="fas fa-plus"></i>',
-//     zoomOutText: '<i class="fas fa-minus"></i>'
-// });
-// map.addControl(zoomControl);
+function createMarkers(iconUrl,iconSize,markersXY,name,markersArray,markersGroup){
 
-// //You can also use CSS to customize the appearance of the zoom control buttons.
-// .leaflet-control-zoom-in, .leaflet-control-zoom-out {
-//     background-color: blue;
-//     color: white;
-//     font-size: 20px;
-//     padding: 5px;
-// }
-//#endregion
+  for(let i=0;i<markersXY.length;i++){
+    var Icon = L.icon({
+      iconUrl:iconUrl,
+      iconSize:iconSize,
+    });
 
+    var Marker = L.marker(markersXY[i],{icon:Icon,opacity:1})
+    var HideAllpopup = `<br><button id='HideAllMarker'>Hide All ${name} </button>`
+    var Foundpopup = "<br><button id='MarkFoundButton'>Mark Found</button>";
+    var NotFoundpopup = "<br><button id='MarkFoundButton'>Mark As Not Found</button>"
+    Marker.bindPopup(HideAllpopup+Foundpopup);
+
+
+    Marker.on("popupopen",function(e){
+      var marker = e.target
+      var button = document.getElementById('MarkFoundButton')
+      var button2 = document.getElementById('HideAllMarker')
+
+      button.addEventListener('click',function(){
+        var opacity = marker.options.opacity;
+        marker.setOpacity(opacity===1? 0.5:1)
+        marker.closePopup();
+        if(opacity===1){
+          var popupContent = HideAllpopup+NotFoundpopup;
+          marker.bindPopup(popupContent);
+        }
+        else{
+          var popupContent = HideAllpopup+Foundpopup;
+          marker.bindPopup(popupContent);
+        }
+      })
+
+      button2.addEventListener('click',function(){
+          hideAllMarkers(markersGroup)
+      })
+
+
+
+    })
+    markersArray[i] = Marker
+  }
+}
+
+
+function hideAllMarkers(groupMarkers){
+    map.removeLayer(groupMarkers);
+}
+
+function updateMarker(){
+
+}
