8317b5c267cbeede399b5c5093de77ff70b2ba33
mavey14 <harveylencio@gmail.com>
2023-05-18 20:59:37 +0800

    change image buffer to image path

--- controllers/comments.js
+++ controllers/comments.js
@@ -9,19 +9,9 @@ const getAllcomments = async(req,res) => {
       var comments = await Comments.find({markerid:req.query.currentMarkerID}).limit(1).exec()
       //console.log('inside of comments.js controller:'+Array.isArray(comments))
 
-      //console.log(comments[0].image)
-      var buffer = comments[0].image.toString('base64')
+     
 
-      // comments.forEach(comment=>{
-      //   comment.image = comment.image.toString('base64')
-
-      //   console.log(isBase64(comment.image))
-      // })
-
-      var buffer = comments[0].image.toString('base64')
-      console.log(isBase64(buffer))
-
-      res.status(StatusCodes.OK).json({comments });
+      //res.status(StatusCodes.OK).json({comments });
     } catch (error) {
       console.log(error)
     }
@@ -29,31 +19,32 @@ const getAllcomments = async(req,res) => {
 
 const saveComment = async(req,res) =>{
     console.clear()
-
+    console.log(req.file)
+    //change image buffer to image path
     try {
         
-        const imagebuffer = await new Promise((resolve, reject) => {
-            fs.readFile(req.file.path, (err, data) => {
-              if (err) {
-                console.log(err);
-                reject(err);
-              } else {
-                resolve(data);
-              }
-            });
-          });
+        // const imagebuffer = await new Promise((resolve, reject) => {
+        //     fs.readFile(req.file.path, (err, data) => {
+        //       if (err) {
+        //         console.log(err);
+        //         reject(err);
+        //       } else {
+        //         resolve(data);
+        //       }
+        //     });
+        //   });
 
-        const newComments = {
-            markerid:req.query.currentMarkerID,
-            message:req.body.message,
-            image:imagebuffer,
-        }
+        // const newComments = {
+        //     markerid:req.query.currentMarkerID,
+        //     message:req.body.message,
+        //     image:imagebuffer,
+        // }
 
 
-        console.log(newComments)
+        // console.log(newComments)
     
-        const comment = await Comments.create(newComments)
-        console.log('inserted')
+        // const comment = await Comments.create(newComments)
+        // console.log('inserted')
     } catch (error) {
         console.log(error)
     }
